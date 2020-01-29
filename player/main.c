d5a54842f7c31314f57956d4f998e501a3aa8159
James Cowgill <jcowgill@debian.org>
2020-01-29 01:03:08 +0000

    Suppress ffmpeg version mismatch error
    Bug-Debian: https://bugs.debian.org/831537
    
    Requiring an exact ffmpeg version is usually not a good idea in a binary
    distribution because:
    - All FFmpeg security updates require a subsequent binNMU of mpv.
    - Debian generated dependencies do not capture this dependency well (at least
      without extra hacking).
    - The requirement itself usually indicates an ABI violation.
    For these reasons, remove the check and assume the current FFmpeg version is
    compatible.
    Bug-Debian: https://bugs.debian.org/831537
    Gbp-Pq: Name 06_ffmpeg-abi.patch

--- player/main.c
+++ player/main.c
@@ -387,18 +387,6 @@ int mp_initialize(struct MPContext *mpctx, char **options)
     if (handle_help_options(mpctx))
         return 1; // help
 
-    if (!print_libav_versions(mp_null_log, 0)) {
-        // This happens only if the runtime FFmpeg version is lower than the
-        // build version, which will not work according to FFmpeg's ABI rules.
-        // This does not happen if runtime FFmpeg is newer, which is compatible.
-        print_libav_versions(mpctx->log, MSGL_FATAL);
-        MP_FATAL(mpctx, "\nmpv was compiled against an incompatible version of "
-                 "FFmpeg/Libav than the shared\nlibrary it is linked against. "
-                 "This is most likely a broken build and could\nresult in "
-                 "misbehavior and crashes.\n\nThis is a broken build.\n");
-        return -1;
-    }
-
 #if HAVE_TESTS
     if (opts->test_mode && opts->test_mode[0])
         return run_tests(mpctx) ? 1 : -1;
